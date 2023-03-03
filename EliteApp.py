140844f4ed1ea69cff5a811b40a2dd1f32f935fe
Uday Kumar Billa <99324984+Udaykumar1208@users.noreply.github.com>
2023-03-03 17:19:32 +0530

    Update EliteApp.py

--- EliteApp.py
+++ EliteApp.py
@@ -27,6 +27,28 @@ upload_path = "uploads/"
 download_path = "downloads/"
 transcript_path = "transcripts/"
 
+##-------------------------------------------------------## Feedback ##----------------------------------------------------------------##
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
+def send_email(name, email, feedback):
+    sender_email = 'billauday9901@gmail.com'
+    sender_password = 'UDAYKUMAR1234'
+    receiver_email = 'bodakiran9652@gmail.com'
+    message = MIMEMultipart()
+    message['From'] = sender_email
+    message['To'] = receiver_email
+    message['Subject'] = 'New Feedback from {} ({})'.format(name, email)
+    message.attach(MIMEText(feedback, 'plain'))
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    text = message.as_string()
+    server.sendmail(sender_email, receiver_email, text)
+    server.quit()
 
 ##--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -151,11 +173,23 @@ def main():
                 for items in keywords:
                                     
                     st.write("▣ ",items[0])
-        
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------#        
 
-                       
-            
-                
+    if st.button('Give Feedback'):
+        feedback_page()                    
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------#        
+
+def feedback_page():
+    st.title('Feedback Form')
+    st.write('Please enter your feedback below:')
+    name = st.text_input('Name')
+    email = st.text_input('Email')
+    feedback = st.text_area('Feedback')
+    if st.button('Submit'):
+        send_email(name, email, feedback)
+        st.write('Thank you for your feedback!')
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------#        
+
 if __name__ == "__main__":
         main()     
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------#        
