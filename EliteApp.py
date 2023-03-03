c7974e3445de8a80922032b474e8342e5c8e679d
Uday Kumar Billa <99324984+Udaykumar1208@users.noreply.github.com>
2023-03-03 18:19:45 +0530

    Update EliteApp.py

--- EliteApp.py
+++ EliteApp.py
@@ -180,7 +180,6 @@ def main():
     if st.button('Give Feedback'):
         feedback_page()                    
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------#        
-@st.cache_data
 def feedback_page():
     st.title('Feedback Form')
     st.write('Please enter your feedback below:')
